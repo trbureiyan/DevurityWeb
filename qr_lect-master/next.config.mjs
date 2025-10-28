cb78a68a93f05a31749a5006e1cae93e126190a6
Arekkazu <alexanderlozada230@gmail.com>
2025-10-28 08:17:46 -0500

    Finished

--- qr_lect-master/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  devIndicators: {
-    buildActivity: false, // Oculta el indicador "compiling..."
-  },
-};
-
-export default nextConfig;
